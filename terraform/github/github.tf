9f7ac8cbcd7ebd33616d9090b3aadd3478f751ad
Azat Bariev <justlumajustmail@gmail.com>
2022-09-27 10:28:41 +0300

    change version

--- terraform/github/github.tf
+++ terraform/github/github.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "~> 1.2.0"
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 4.0"
+      version = "5.3.0"
     }
   }
 }
