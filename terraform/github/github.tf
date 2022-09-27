f23e52d798c4018237ce707aa6b5625a0c859f07
Azat Bariev <justlumajustmail@gmail.com>
2022-09-27 10:22:02 +0300

    add github terraform

---
+++ terraform/github/github.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = "~> 1.2.0"
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.token # or `GITHUB_TOKEN`
+}
+
+#Create and initialise a public GitHub Repository with MIT license and a Visual Studio .gitignore file (incl. issues and wiki)
+resource "github_repository" "labs" {
+  name               = "labs"
+  description        = "Github repository for devops course"
+  visibility         = "public"
+  allow_rebase_merge = false
+  allow_squash_merge = false
+}
+
+#Set default branch 'master'
+resource "github_branch_default" "master" {
+  repository = github_repository.labs.name
+  branch     = "master"
+}
+
+#Create branch protection rule to protect the default branch. (Use "github_branch_protection_v3" resource for Organisation rules)
+resource "github_branch_protection" "default" {
+  repository_id                   = github_repository.repo.id
+  pattern                         = github_branch_default.master.branch
+  require_conversation_resolution = true
+  enforce_admins                  = true
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+}
