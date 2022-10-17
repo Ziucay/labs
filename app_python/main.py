8563772a0c210ff9e5793aa8b4e64443143df173
Azat Bariev <justlumajustmail@gmail.com>
2022-10-17 14:49:11 +0300

    Update main.py

--- app_python/main.py
+++ app_python/main.py
@@ -1,16 +1,19 @@
+from prometheus_fastapi_instrumentator import Instrumentator
 from fastapi import FastAPI
 from datetime import datetime
 import pytz
 
 app = FastAPI()
 
+# Expose /metrics endpoint for prometheus
+@app.on_event("startup")
+async def startup():
+    Instrumentator().instrument(app).expose(app)
 
 def moscow_time():
     timezone = pytz.timezone('Europe/Moscow')
 
     return datetime.now(timezone).isoformat()
-
-
 @app.get("/")
 async def root():
     return {"time": f"{moscow_time()}"}
